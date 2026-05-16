5822abe0db5f53943887538ff0c7505f3c4978a4
Daniel Silveira <danieldasilvasilveira2002@gmail.com>
2026-05-16 13:25:10 -0300

    site finalizado

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -4,9 +4,9 @@ export const BUSINESS = {
   tagline: 'Chopp gelado na sua porta, sem preocupações',
   whatsapp: '5521968462709', // formato internacional, sem '+', sem espaços
   whatsappMessage: 'Olá! Quero um orçamento de chopp para minha festa.',
-  email: 'contato@sschopp.com.br',
-  instagram: 'https://instagram.com/sschopp',
-  city: 'São Paulo / SP',
+  email: 'sschoppexpresso@gmail.com',
+  instagram: 'https://www.instagram.com/ss_chopp/',
+  city: 'Rio de Janeiro / RJ',
 }
 
 export const buildWhatsAppUrl = (msg = BUSINESS.whatsappMessage) =>
